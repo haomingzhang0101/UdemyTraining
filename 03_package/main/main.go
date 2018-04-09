c67261fdfe80f69ef4ffd989c28b0316d1608b4b
Haoming Zhang <haomingzhang0101@gmail.com>
2018-04-09 20:08:22 +0800

    package and exported/unexported

---
+++ 03_package/main/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/haomingzhang0101/UdemyTraining/03_package/stringutil"
+)
+
+func main() {
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
+	fmt.Println(stringutil.MyName)
+}
